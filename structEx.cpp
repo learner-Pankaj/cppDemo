ddb77c8a2d250f21cdffb3fc0623dbebe94be4ab
Pankaj <ydvpnkj28@gmail.com>
2025-03-27 16:22:39 +0530

    structure

---
+++ structEx.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+struct person{
+    string name;
+    int age;
+    float height;
+    double salary;
+};
+
+int main(){
+    person pVar;
+    pVar.name = "Pankaj";
+    pVar.age = 28;
+    pVar.height = 170;
+    pVar.salary = 980000;
+
+    cout << "Details of the Person.\n";
+    cout << "Name : "<<pVar.name << endl;
+    cout << "Age : "<<pVar.age << endl;
+    cout << "Height : "<<pVar.height << endl;
+    cout << "Salary : "<<pVar.salary << endl;
+    return 0;
+}
